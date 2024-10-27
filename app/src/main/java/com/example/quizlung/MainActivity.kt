b36a63f0b78beec6626ae0e9d64f0b3003d90020
Rafael Vonseka Sianturi <rafaelsianturi19@gmail.com>
2024-10-27 19:51:41 +0700

    Memperbaiki beberapa source code

--- app/src/main/java/com/example/quizlung/MainActivity.kt
+++ app/src/main/java/com/example/quizlung/MainActivity.kt
@@ -1,39 +1,44 @@
 package com.example.quizlung
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private fun setupCardListeners() {
+        findViewById<CardView>(R.id.cvCompSci).setOnClickListener {
+            startActivity(Intent(this, CompSciActivity::class.java))
+        }
+        findViewById<CardView>(R.id.cvSocKnow).setOnClickListener {
+            startActivity(Intent(this, SocknowActivity::class.java))
+        }
+        findViewById<CardView>(R.id.cvNatSci).setOnClickListener {
+            startActivity(Intent(this, NatScienceActivity::class.java))
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val compSci: CardView = findViewById(R.id.cvCompSci)
-        compSci.setOnClickListener {
-            val intent = Intent(this@MainActivity, CompSciActivity::class.java)
-            startActivity(intent)
-        }
 
-        val socKnow: CardView = findViewById(R.id.cvSocKnow)
-        socKnow.setOnClickListener {
-            val intent = Intent(this@MainActivity, SocknowActivity::class.java)
-            startActivity(intent)
-        }
 
-        val natSci: CardView = findViewById(R.id.cvNatSci)
-        natSci.setOnClickListener {
-            val intent = Intent(this@MainActivity, NatScienceActivity::class.java)
-            startActivity(intent)
-        }
+        setupCardListeners()
     }
 }
